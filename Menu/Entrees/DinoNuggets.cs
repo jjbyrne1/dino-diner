64885d98a87c0cefe2cf5aea0f34e9c5902ae5fe
Jason Byrne <jjbyrne@ksu.edu>
2019-09-13 12:27:02 -0500

    all classes filled out and completed, test cases aren't updated

---
+++ Menu/Entrees/DinoNuggets.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DinoDiner.Menu.Entrees
+{
+    public class DinoNuggets
+    {
+        private uint numOfNuggets = 6;
+
+        public double Price { get; set; }
+        public uint Calories { get; set; }
+
+        public DinoNuggets()
+        {
+            this.Price = 4.25 + .25*(numOfNuggets-6);
+            this.Calories = 59 * numOfNuggets;
+        }
+
+        public List<string> Ingredients
+        {
+            get
+            {
+                return new List<string>() { "Chicken Nugget" };
+            }
+        }
+
+        public void AddNugget()
+        {
+            this.numOfNuggets++;
+        }
+    }
+}
---
+++ Menu/Entrees/Pterodactyl Wings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DinoDiner.Menu.Entrees
+{
+    public class PterodactylWings
+    {
+        public double Price { get; set; }
+        public uint Calories { get; set; }
+
+        public PterodactylWings()
+        {
+            this.Price = 7.21;
+            this.Calories = 318;
+        }
+
+        public List<string> Ingredients
+        {
+            get
+            {
+                return new List<string>() { "Chicken" , "Wing Sauce" };
+            }
+        }
+    }
+}
